ebbf164666567e02908f2e6970299bd586b21505
Thiago <168425342+Thiagofrancaveloso@users.noreply.github.com>
2024-05-27 09:19:09 -0300

    Update PotenciaNumeroDois.java

--- PotenciaNumeroDois.java
+++ PotenciaNumeroDois.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner ref = new Scanner(System.in);
 
         System.out.println("Digite um número para verificar se é uma potência de 2:");
         int numero = ref.nextInt();
 
-        if (isPotenciaDeDois(numero)) {
+        boolean isPotenciaDeDois = (numero & (numero - 1)) == 0 && numero != 0;
+
+        if (isPotenciaDeDois) {
             System.out.println(numero + " é uma potência de 2.");
         } else {
             System.out.println(numero + " não é uma potência de 2.");
         }
-    }
-
-    public static boolean isPotenciaDeDois(int numero) {
-        return (numero & (numero - 1)) == 0 && numero != 1;
     }
 }
