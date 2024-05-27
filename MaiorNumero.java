5d53c9f49db371375c8429c322f63fd3f09cf414
Thiago <168425342+Thiagofrancaveloso@users.noreply.github.com>
2024-05-27 09:13:39 -0300

    Create MaiorNumero.java

---
+++ MaiorNumero.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class maiorNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        int numeroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int numeroDois = scanner.nextInt();
+
+        System.out.println("Digite o terceiro número:");
+        int numeroTres = scanner.nextInt();
+
+        int maiorNumero = numeroUm;
+        
+        if (numeroDois > maiorNumero) {
+            maiorNumero = numeroDois;
+        }
+
+        if (numeroTres > maiorNumero) {
+            maiorNumero = numeroTres;
+        }
+        
+        System.out.println("O maior número digitado é: " + maiorNumero);
+
+    }
+}
